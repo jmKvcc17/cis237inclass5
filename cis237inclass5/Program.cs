942abb651f63aa436d374e54a419ee3eb0aea388
Jesse Meachum <jmeachum17@gmail.com>
2017-03-22 19:36:05 -0400

    Added stuff

--- cis237inclass5/Program.cs
+++ cis237inclass5/Program.cs
@@ -10,6 +10,18 @@ namespace cis237inclass5
     {
         static void Main(string[] args)
         {
+            // make new instance of the car class
+            // Probably going to be CarsFInitialLName for you.
+            CarsJMeachumEntities carsTestEntities = new CarsJMeachumEntities();
+
+            // List out all of the cars in the table
+            Console.WriteLine("Print the list");
+
+            foreach(Car car in carsTestEntities.Cars)
+            {
+                Console.WriteLine(car.id + " " + car.make + " " + car.model);
+            }
+
         }
     }
 }
